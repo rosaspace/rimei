fe164fb49f18ad091a0e5937159a4138651e9cce
rosaspace <rosaspace@126.com>
2025-07-14 16:44:27 -0500

    update contabo db host

--- rimei/settings.py
+++ rimei/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'NAME': os.getenv("DB_NAME", "ljdb"),
         'USER': os.getenv("DB_USER", "lj"),
         'PASSWORD': os.getenv("DB_PASSWORD", "mengxiang0425"),
-        'HOST': os.getenv("DB_HOST", "db"),
+        'HOST': '172.17.0.1',
         'PORT': '5432',
     }
 }
